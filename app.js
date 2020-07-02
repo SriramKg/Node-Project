c05dc445b4d15381bdbbe59e1507ad2a035f6e91
Sriram Gopinath <kgsriram99@gmail.com>
2020-07-02 12:33:27 +0530

    Cookies

--- app.js
+++ app.js
@@ -31,31 +31,45 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-12345-67890'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  var authheader = req.headers.authorization;
-  if(!authheader){
-    var err = new Error('You are not permitted to Authenticate');
-    res.setHeader("WWW-Authenticate", 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-
-  var auth = new Buffer(authheader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username === 'admin' && password ==='password'){
-    next();
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user) {
+    var authheader = req.headers.authorization;
+    if(!authheader){
+      var err = new Error('You are not permitted to Authenticate');
+      res.setHeader("WWW-Authenticate", 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  
+    var auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+  
+    if(username === 'admin' && password ==='password'){
+      res.cookie('user', 'admin', { signed : true} );
+      next();
+    }
+    else{
+      var err = new Error('You are not permitted to Authenticate');
+      res.setHeader("WWW-Authenticate", 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
   else{
-    var err = new Error('You are not permitted to Authenticate');
-    res.setHeader("WWW-Authenticate", 'Basic');
-    err.status = 401;
-    return next(err);
+    if(req.signedCookies.user === 'admin') {
+      next();
+    }
+    else{
+      var err = new Error('You are not permitted to Authenticate');
+      err.status = 401;
+      return next(err);
+    }
   }
+
 }
 app.use(auth)
 
