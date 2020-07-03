fa697c33675b31ad867a6f986cd7b5cd9da3cd18
Sriram Gopinath <kgsriram99@gmail.com>
2020-07-03 11:15:27 +0530

    Express sessions part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var fileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -31,11 +33,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-12345-67890'));
+//app.use(cookieParser('12345-67890-12345-67890'));
+
+app.use(session({
+  name : 'session',
+  secret : '12345-67890-12345-67890',
+  saveUninitialized : false,
+  resave : false,
+  store : new fileStore()
+}));
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user) {
+  console.log(req.session);
+
+  if(!req.session.user) {
+
     var authheader = req.headers.authorization;
     if(!authheader){
       var err = new Error('You are not permitted to Authenticate');
@@ -49,7 +61,7 @@ function auth(req, res, next) {
     var password = auth[1];
   
     if(username === 'admin' && password ==='password'){
-      res.cookie('user', 'admin', { signed : true} );
+      req.session.user = 'admin';
       next();
     }
     else{
@@ -60,7 +72,7 @@ function auth(req, res, next) {
     }
   }
   else{
-    if(req.signedCookies.user === 'admin') {
+    if(req.session.user === 'admin') {
       next();
     }
     else{
