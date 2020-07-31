d1cdae36d4b1d555e4aa9631f747f00c356bf062
Sriram Gopinath <kgsriram99@gmail.com>
2020-07-31 11:35:40 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-12345-67890',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '378430989804391',
+        clientSecret : '2d92093768a526a89fc97a958dc5da26'
+    }
 }
